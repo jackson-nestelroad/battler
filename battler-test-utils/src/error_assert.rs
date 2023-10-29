afbd8df7c9a3def8e7b2f2080e41c3c4377bf122
Jackson Nestelroad <jackson@nestelroad.com>
2023-10-28 21:14:19 -0500

    Add error message to error asserts

--- battler-test-utils/src/error_assert.rs
+++ battler-test-utils/src/error_assert.rs
@@ -1,13 +1,29 @@
+use std::fmt::Debug;
+
 use battler::common::Error;
 
 /// [`assert`]s that the result is an [`Error`] with the given message.
 #[track_caller]
-pub fn assert_error_message<T>(result: Result<T, Error>, message: &str) {
-    assert!(result.is_err_and(|err| err.message() == message))
+pub fn assert_error_message<T>(result: Result<T, Error>, message: &str)
+where
+    T: Debug,
+{
+    assert!(
+        result.as_ref().is_err_and(|err| err.message() == message),
+        "result is {result:?}, not an error with message \"{message}\"",
+    )
 }
 
 /// [`assert`]s that the result is an [`Error`] that contains the given message.
 #[track_caller]
-pub fn assert_error_message_contains<T>(result: Result<T, Error>, message: &str) {
-    assert!(result.is_err_and(|err| err.message().contains(message)))
+pub fn assert_error_message_contains<T>(result: Result<T, Error>, message: &str)
+where
+    T: Debug,
+{
+    assert!(
+        result
+            .as_ref()
+            .is_err_and(|err| err.message().contains(message)),
+        "result is {result:?}, not an error with a message that contains \"{message}\""
+    )
 }
