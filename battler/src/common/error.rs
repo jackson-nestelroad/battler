96b2e319d2ae74d36514ce8d17683a65f1c830fc
Jackson Nestelroad <jackson@nestelroad.com>
2023-10-29 23:04:39 -0500

    Add assertions for error messages

--- battler/src/common/error.rs
+++ battler/src/common/error.rs
@@ -4,6 +4,7 @@ use std::{
     fmt::{
         format,
         Arguments,
+        Debug,
         Display,
         Formatter,
         Result as DisplayResult,
@@ -240,6 +241,34 @@ impl<T> WrapResultError<T> for Option<T> {
             Some(val) => Ok(val),
         }
     }
+}
+
+#[cfg(test)]
+/// [`assert`]s that the result is an [`Error`] with the given message.
+#[track_caller]
+pub fn assert_error_message<T>(result: Result<T, Error>, message: &str)
+where
+    T: Debug,
+{
+    assert!(
+        result.as_ref().is_err_and(|err| err.message() == message),
+        "result is {result:?}, not an error with message \"{message}\"",
+    )
+}
+
+#[cfg(test)]
+/// [`assert`]s that the result is an [`Error`] that contains the given message.
+#[track_caller]
+pub fn assert_error_message_contains<T>(result: Result<T, Error>, message: &str)
+where
+    T: Debug,
+{
+    assert!(
+        result
+            .as_ref()
+            .is_err_and(|err| err.message().contains(message)),
+        "result is {result:?}, not an error with a message that contains \"{message}\""
+    )
 }
 
 #[cfg(test)]
