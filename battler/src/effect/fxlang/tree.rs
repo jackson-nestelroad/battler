c692a9bbca8fa235bdec5bf30e0bac5d8f4857b5
Jackson Nestelroad <jackson@nestelroad.com>
2024-08-23 20:32:39 -0700

    Fix Return statement grammar comment

--- battler/src/effect/fxlang/tree.rs
+++ battler/src/effect/fxlang/tree.rs
@@ -218,7 +218,7 @@ pub struct ForEachStatement {
     pub range: Value,
 }
 
-/// ReturnStatement -> "return" Expr
+/// ReturnStatement -> "return" (Expr)
 #[derive(Debug, PartialEq, Eq)]
 #[repr(transparent)]
 pub struct ReturnStatement(pub Option<Expr>);
