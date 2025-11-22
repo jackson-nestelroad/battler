c00ce3e0667b435eb144651e353664f8744473d7
Jackson Nestelroad <jackson@nestelroad.com>
2025-11-22 12:03:00 -0600

    Add doubles battle scenario

--- battler-ai/tests/tests/gemini_test.rs
+++ battler-ai/tests/tests/gemini_test.rs
@@ -10,3 +10,12 @@ async fn picks_valid_move() {
     let mut gemini = Gemini::default();
     assert_matches::assert_matches!(scenario.validate_expected_result(&mut gemini).await, Ok(()));
 }
+
+#[tokio::test]
+async fn picks_valid_move_for_double_battle() {
+    let scenario = Scenario::from_scenarios_dir("simple_double_battle.json")
+        .await
+        .unwrap();
+    let mut gemini = Gemini::default();
+    assert_matches::assert_matches!(scenario.validate_expected_result(&mut gemini).await, Ok(()));
+}
