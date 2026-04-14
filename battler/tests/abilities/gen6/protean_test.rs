a95ffeceed40341e2f52dd28972f407d350cbe3c
Jackson Nestelroad <jackson@nestelroad.com>
2026-04-14 14:53:31 -0500

    Add Protean x Mega Evolution test

--- battler/tests/abilities/gen6/protean_test.rs
+++ battler/tests/abilities/gen6/protean_test.rs
@@ -21,9 +21,11 @@ fn team() -> Result<TeamData> {
                     "name": "Greninja",
                     "species": "Greninja",
                     "ability": "Protean",
+                    "item": "Greninjite",
                     "moves": [
                         "Flamethrower",
-                        "Water Gun"
+                        "Water Gun",
+                        "Tackle"
                     ],
                     "nature": "Hardy",
                     "level": 100
@@ -40,6 +42,7 @@ fn make_battle(seed: u64, team_1: TeamData, team_2: TeamData) -> Result<PublicCo
         .with_seed(seed)
         .with_team_validation(false)
         .with_pass_allowed(true)
+        .with_mega_evolution(true)
         .with_speed_sort_tie_resolution(CoreBattleEngineSpeedSortTieResolution::Keep)
         .add_player_to_side_1("player-1", "Player 1")
         .add_player_to_side_2("player-2", "Player 2")
@@ -85,6 +88,44 @@ fn protean_changes_type_to_match_move_once() {
             "split|side:0",
             "damage|mon:Greninja,player-1,1|health:68/254",
             "damage|mon:Greninja,player-1,1|health:27/100",
+            "residual",
+            "turn|turn:3"
+        ]"#,
+    )
+    .unwrap();
+    assert_logs_since_turn_eq(&battle, 1, &expected_logs);
+}
+
+#[test]
+fn protean_resets_after_mega_evolution() {
+    let mut battle = make_battle(0, team().unwrap(), team().unwrap()).unwrap();
+    assert_matches::assert_matches!(battle.start(), Ok(()));
+
+    assert_matches::assert_matches!(battle.set_player_choice("player-1", "move 0"), Ok(()));
+    assert_matches::assert_matches!(battle.set_player_choice("player-2", "pass"), Ok(()));
+    assert_matches::assert_matches!(battle.set_player_choice("player-1", "move 2,mega"), Ok(()));
+    assert_matches::assert_matches!(battle.set_player_choice("player-2", "pass"), Ok(()));
+
+    let expected_logs = serde_json::from_str::<Vec<LogMatch>>(
+        r#"[
+            "move|mon:Greninja,player-1,1|name:Flamethrower|target:Greninja,player-2,1",
+            "typechange|mon:Greninja,player-1,1|types:Fire",
+            "resisted|mon:Greninja,player-2,1",
+            "split|side:1",
+            "damage|mon:Greninja,player-2,1|health:175/254",
+            "damage|mon:Greninja,player-2,1|health:69/100",
+            "residual",
+            "turn|turn:2",
+            "continue",
+            "split|side:0",
+            ["specieschange", "player-1", "Greninja-Mega"],
+            ["specieschange", "player-1", "Greninja-Mega"],
+            "mega|mon:Greninja,player-1,1|species:Greninja-Mega|from:item:Greninjite",
+            "move|mon:Greninja,player-1,1|name:Tackle|target:Greninja,player-2,1",
+            "typechange|mon:Greninja,player-1,1|types:Normal",
+            "split|side:1",
+            "damage|mon:Greninja,player-2,1|health:90/254",
+            "damage|mon:Greninja,player-2,1|health:36/100",
             "residual",
             "turn|turn:3"
         ]"#,
